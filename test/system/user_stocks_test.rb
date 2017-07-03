83c14b73f77ef120b2ad38f6e9003525ddcfdefe
Otavio Augusto <tavioalves@gmail.com>
2017-07-03 17:31:39 -0300

    Adição da associação de User com Stock

---
+++ test/system/user_stocks_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UserStocksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit user_stocks_url
+  #
+  #   assert_selector "h1", text: "UserStock"
+  # end
+end
