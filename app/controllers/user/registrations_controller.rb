5734b1bdc5fdb5b1f458904fe331538713b375ba
Otavio Augusto <tavioalves@gmail.com>
2017-07-04 15:56:06 -0300

    Atualização das views e navigations

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,7 @@
+class User::RegistrationsController < Devise::RegistrationsController
+     before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+end
